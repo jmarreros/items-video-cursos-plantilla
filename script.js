2dc2a4457e7d93021e6a747aa35bd5c014741659
Jhon Marreros Guzman <j_marreros@hotmail.com>
2018-01-01 12:13:45 -0500

    Agregado funcionalidad de añadir nuevo

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const template = `
+<li class="item">
+	<div class="links">
+		<a href="#" class="item-link"></a>
+		<a href="#" class="arrow"></a>
+	</div>
+	<div class="controls">
+		<p class="control-name">
+			<label>Name</label><input type="text" value=""/>
+		</p>
+		<p class="control-check-header">
+			<input type="checkbox"><label>Is header?</label>
+		</p>
+		<p class="control-type">
+			<label>Type</label>
+			<select>
+				<option value="youtube" selected>Youtube</option>
+				<option value="vimeo">Vimeo</option>
+				<option value="custom">custom</option>
+			</select>
+		</p>
+		<p class="control-code">
+			<label>Code</label><textarea cols="30" rows="4"></textarea>						
+		</p>
+		<p class="control-check-lock">
+			<input type="checkbox">
+			<label>Locked for guests?</label>
+		</p>
+		<hr>
+		<div class="control-options">
+			<div><a href="#" class="remove">Remove</a> | <a href="#" class="cancel">Cancel</a></div>
+			<div><a href="#" class="preview">Preview</a></div>
+		</div>
+	</div>
+</li>`;
+
 const items = document.getElementById('list-items');
 
 // Click Events for the link show/hide controls
@@ -48,14 +84,33 @@ items.addEventListener( 'keyup', function(e){
 });
 
 
-const item = document.querySelector('.item');
+// Adding new item
+const addnew =  document.getElementById('add-item');
 
-update_ids( item, 3 );
+addnew.addEventListener('click', function(){
+	const listhtml = document.createRange().createContextualFragment(template);
+	items.appendChild(listhtml);
 
+	const item = items.lastChild;
+	update_ids(item, items.childElementCount);
+
+	// console.log(items.childElementCount);
+});
+
+
+// function to udpate ids attributes for the HTML item
 function update_ids( item, id){
 	item.setAttribute('data-id', id);
 	item.querySelector('.control-name label').setAttribute('for', `name-${id}`);
 	item.querySelector('.control-name input').setAttribute('id', `name-${id}`);
+	item.querySelector('.control-check-header label').setAttribute('for', `isheader-${id}`);
+	item.querySelector('.control-check-header input').setAttribute('id', `isheader-${id}`);
+	item.querySelector('.control-type label').setAttribute('for', `type-${id}`);
+	item.querySelector('.control-type select').setAttribute('id', `type-${id}`);
+	item.querySelector('.control-code label').setAttribute('for', `code-${id}`);
+	item.querySelector('.control-code textarea').setAttribute('id', `code-${id}`);
+	item.querySelector('.control-check-lock label').setAttribute('for', `islock-${id}`);
+	item.querySelector('.control-check-lock input').setAttribute('id', `islock-${id}`);
 }
 
 
